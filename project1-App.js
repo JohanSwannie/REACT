9ef8acbc0bcd949f3aefdc85d8e6551a3c6e2e62
Johan Swan <53649803+JohanSwannie@users.noreply.github.com>
2022-03-18 21:05:01 +1300

    Create project1-App.js

---
+++ project1-App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Header from "./Header";
+import Tasks from "./Tasks";
+
+function App() {
+  return (
+    <div className="ui container">
+      <Header />
+      <Tasks />
+    </div>
+  );
+};
+
+export default App;
