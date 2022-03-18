bc024a0928fb624e6b7485ef568336eecbc487b9
Johan Swan <53649803+JohanSwannie@users.noreply.github.com>
2022-03-18 21:05:46 +1300

    Create project1-Header.js

---
+++ project1-Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+
+const Header = () => {
+  return (
+    <div className="ui fixed menu">
+      <div className="ui container center">
+        <h3>Learning REACTJS</h3>
+        <p style={{ "color": "#8b0000"}}>Not easy stuff but I will endure</p>
+     </div>
+    </div>
+  );
+};
+
+
+export default Header;
