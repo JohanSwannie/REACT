7ed7779ea41bb0157fc49904e589929ac3b477f9
Johan Swan <53649803+JohanSwannie@users.noreply.github.com>
2022-03-19 22:58:13 +1300

    Create project2-goodfoods.js

---
+++ project2-goodfoods.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+const Header = () => {
+  return (
+    <div id='subbox' className="ui fixed menu">
+      <div className="ui container center">
+        <h3>Discover some good and healthy foods to eat</h3>
+     </div>
+    </div>
+  );
+};
+
+export default Header;
