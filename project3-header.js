b0465589eb6ed76f30eb0b193a57785afee19f0b
Johan Swan <53649803+JohanSwannie@users.noreply.github.com>
2022-03-21 20:27:03 +1300

    Create project3-header.js

---
+++ project3-header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+const Header = () => {
+  return (
+    <div id='subbox' className="ui fixed menu">
+      <div className="ui container center">
+        <h3>TOGGLE BUTTON MAGIC</h3>
+     </div>
+    </div>
+  );
+};
+
+export default Header;
