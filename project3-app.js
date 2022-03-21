7146a7044b9ef04e08e31c68f511c2bdf50940cc
Johan Swan <53649803+JohanSwannie@users.noreply.github.com>
2022-03-21 20:26:23 +1300

    Create project3-app.js

---
+++ project3-app.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Header from "./Header";
+import ToggleButton from "./ToggleButton";
+
+function App() {
+  return (
+    <div id="mainbox" className="ui container">
+      <Header />
+      <ToggleButton />
+    </div>
+  );
+};
+
+export default App;
