f07cbd353133e6bed17120c915b873a0415e424c
Johan Swan <53649803+JohanSwannie@users.noreply.github.com>
2022-03-24 21:13:14 +1300

    Create project4-header.js

---
+++ project4-header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+const Header = () => {
+  return (
+    <div id='subbox' className="ui fixed menu">
+      <div className="ui container center">
+        <h3>CSS Magic</h3>
+     </div>
+    </div>
+  );
+};
+
+export default Header;
