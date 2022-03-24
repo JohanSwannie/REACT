ba0a87053c71711730c631af2347c5cfeab0f6ec
Johan Swan <53649803+JohanSwannie@users.noreply.github.com>
2022-03-24 20:37:09 +1300

    Create project4-app.js

---
+++ project4-app.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Header from "./Header";
+import CssMagic from "./CssMagic";
+
+function App() {
+  return (
+    <div id="mainbox" className="ui container">
+      <Header />
+      <CssMagic />
+    </div>
+  );
+};
+
+export default App;
