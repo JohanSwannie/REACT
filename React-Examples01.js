8f0e6f20957ca7c9443242ada3bf75a19e80e5df
Johan Swan <53649803+JohanSwannie@users.noreply.github.com>
2022-10-21 08:40:30 +1100

    Update React-Examples01.js

--- React-Examples01.js
+++ React-Examples01.js
@@ -1263,3 +1263,381 @@ const loginAction = () => {
 };
 
 store.dispatch(loginAction());
+
+{/* HANDLE AN ACTION IN THE STORE */}
+
+const defaultState = {
+  login: false
+};
+
+const reducer = (state = defaultState, action) => {
+  if (action.type === "LOGIN") {
+    return {
+      login: true
+    };
+  } else {
+    return state;
+  }
+};
+
+const store = Redux.createStore(reducer);
+
+const loginAction = () => {
+  return {
+    type: 'LOGIN'
+  }
+};
+
+{/* USE A SWITCH STATEMENT TO HANDLE MULTIPLE ACTIONS */}
+
+const defaultState = {
+  authenticated: false
+};
+
+const authReducer = (state = defaultState, action) => {
+  switch(action.type) {
+    case 'LOGIN':
+      return {
+        authenticated: true
+      }
+      break;
+    case 'LOGOUT':
+    return {
+      authenticated: false
+    }
+    break;
+    default:
+      return defaultState;
+  }
+};
+
+const store = Redux.createStore(authReducer);
+
+const loginUser = () => {
+  return {
+    type: 'LOGIN'
+  }
+};
+
+const logoutUser = () => {
+  return {
+    type: 'LOGOUT'
+  }
+};
+
+{/* USE CONST FOR ACTION TYPES */}
+
+const LOGIN = 'LOGIN';
+const LOGOUT = 'LOGOUT';
+
+const defaultState = {
+  authenticated: false
+};
+
+const authReducer = (state = defaultState, action) => {
+
+  switch (action.type) {
+    case LOGIN:
+      return {
+        authenticated: true
+      }
+    case LOGOUT:
+      return {
+        authenticated: false
+      }
+    default:
+      return state;
+  }
+};
+
+const store = Redux.createStore(authReducer);
+
+const loginUser = () => {
+  return {
+    type: LOGIN
+  }
+};
+
+const logoutUser = () => {
+  return {
+    type: LOGOUT
+  }
+};
+
+{/* REGISTER A STORE LISTENER */}
+
+const ADD = 'ADD';
+
+const reducer = (state = 0, action) => {
+  switch(action.type) {
+    case ADD:
+      return state + 1;
+    default:
+      return state;
+  }
+};
+
+const store = Redux.createStore(reducer);
+
+let count = 0;
+
+store.subscribe(function () {
+  count += 1;
+})
+
+store.dispatch({type: ADD});
+console.log(count);
+store.dispatch({type: ADD});
+console.log(count);
+store.dispatch({type: ADD});
+console.log(count);
+
+{/* COMBINE MULTIPLE REDUCERS */}
+
+const INCREMENT = 'INCREMENT';
+const DECREMENT = 'DECREMENT';
+
+const counterReducer = (state = 0, action) => {
+  switch(action.type) {
+    case INCREMENT:
+      return state + 1;
+    case DECREMENT:
+      return state - 1;
+    default:
+      return state;
+  }
+};
+
+const LOGIN = 'LOGIN';
+const LOGOUT = 'LOGOUT';
+
+const authReducer = (state = {authenticated: false}, action) => {
+  switch(action.type) {
+    case LOGIN:
+      return {
+        authenticated: true
+      }
+    case LOGOUT:
+      return {
+        authenticated: false
+      }
+    default:
+      return state;
+  }
+};
+
+const rootReducer = Redux.combineReducers({
+  count: counterReducer,
+  auth: authReducer
+})
+
+const store = Redux.createStore(rootReducer);
+
+{/* SEND ACTION DATA TO THE STORE */}
+
+const ADD_NOTE = 'ADD_NOTE';
+
+const notesReducer = (state = 'Initial State', action) => {
+  switch(action.type) {
+    case ADD_NOTE:
+      return action.text;
+    default:
+      return state;
+  }
+};
+
+const addNoteText = (note) => {
+  return {
+    type: ADD_NOTE,
+    text: note
+  };
+};
+
+const store = Redux.createStore(notesReducer);
+
+console.log(store.getState());
+store.dispatch(addNoteText('Hello!'));
+console.log(store.getState());
+
+{/* USE MIDDLEWARE TO HANDLE ASYNCHRONOUS ACTIONS */}
+
+const REQUESTING_DATA = "REQUESTING_DATA";
+const RECEIVED_DATA = "RECEIVED_DATA";
+
+const requestingData = () => {
+  return { type: REQUESTING_DATA };
+};
+const receivedData = data => {
+  return { type: RECEIVED_DATA, users: data.users };
+};
+
+const handleAsync = () => {
+  return function(dispatch) {
+    dispatch(requestingData());
+    setTimeout(function() {
+      let data = {
+        users: ["Jeff", "William", "Alice"]
+      };
+      dispatch(receivedData(data));
+    }, 2500);
+  };
+};
+
+const defaultState = {
+  fetching: false,
+  users: []
+};
+
+const asyncDataReducer = (state = defaultState, action) => {
+  switch (action.type) {
+    case REQUESTING_DATA:
+      return {
+        fetching: true,
+        users: []
+      };
+    case RECEIVED_DATA:
+      return {
+        fetching: false,
+        users: action.users
+      };
+    default:
+      return state;
+  }
+};
+
+const store = Redux.createStore(
+  asyncDataReducer,
+  Redux.applyMiddleware(ReduxThunk.default)
+);
+
+{/* WRITE A COUNTER WITH REDUX */}
+
+const INCREMENT = "INCREMENT";
+const DECREMENT = "DECREMENT";
+const counterReducer = (state = 0, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return state + 1;
+
+    case DECREMENT:
+      return state - 1;
+
+    default:
+      return state;
+  }
+};
+
+const incAction = () => {
+  return {
+    type: INCREMENT
+  };
+};
+
+const decAction = () => {
+  return {
+    type: DECREMENT
+  };
+};
+
+const store = Redux.createStore(counterReducer);
+
+{/* NEVER MUTATE STATE */}
+
+const ADD_TO_DO = "ADD_TO_DO";
+
+const todos = [
+  "Go to the store",
+  "Clean the house",
+  "Cook dinner",
+  "Learn to code"
+];
+
+const immutableReducer = (state = todos, action) => {
+  switch (action.type) {
+    case ADD_TO_DO:
+
+      return state.concat(action.todo);
+
+    default:
+      return state;
+  }
+};
+
+const addToDo = todo => {
+  return {
+    type: ADD_TO_DO,
+    todo
+  };
+};
+
+const store = Redux.createStore(immutableReducer);
+
+{/* USE THE SPREAD OPERATOR ON ARRAYS */}
+
+const immutableReducer = (state = ["Do not mutate state!"], action) => {
+  switch (action.type) {
+    case "ADD_TO_DO":
+      let arr = [...state, action.todo];
+      return arr;
+    default:
+      return state;
+  }
+};
+
+const addToDo = todo => {
+  return {
+    type: "ADD_TO_DO",
+    todo
+  };
+};
+
+const store = Redux.createStore(immutableReducer);
+
+{/* REMOVE AN ITEM FROM AN ARRAY */}
+
+const immutableReducer = (state = [0, 1, 2, 3, 4, 5], action) => {
+  switch (action.type) {
+    case "REMOVE_ITEM":
+      return [
+        ...state.slice(0, action.index),
+        ...state.slice(action.index + 1, state.length)
+      ];
+    default:
+      return state;
+  }
+};
+
+const removeItem = index => {
+  return {
+    type: "REMOVE_ITEM",
+    index
+  };
+};
+
+const store = Redux.createStore(immutableReducer);
+
+{/* COPY AN OBJECT WITH OBJECT.ASSIGN */}
+
+const defaultState = {
+  user: "CamperBot",
+  status: "offline",
+  friends: "732,982",
+  community: "freeCodeCamp"
+};
+
+const immutableReducer = (state = defaultState, action) => {
+  switch (action.type) {
+    case "ONLINE":
+      return Object.assign({}, state, { status: "online" });
+    default:
+      return state;
+  }
+};
+
+const wakeUp = () => {
+  return {
+    type: "ONLINE"
+  };
+};
+
+const store = Redux.createStore(immutableReducer);
