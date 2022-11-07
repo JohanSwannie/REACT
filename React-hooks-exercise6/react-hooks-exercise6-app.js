74b7fe85c319d331caf0a3dd4c63d68946798f21
Johan Swan <53649803+JohanSwannie@users.noreply.github.com>
2022-11-08 07:53:44 +1100

    Update react-hooks-exercise6-app.js

--- React-hooks-exercise6/react-hooks-exercise6-app.js
+++ React-hooks-exercise6/react-hooks-exercise6-app.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import FunctionComponentWithContext from './FunctionComponentWithContext';
 import ClassComponentWithContext from './ClassComponentWithContext';
+import FunctionComponentWithContext from './FunctionComponentWithContext';
 import './App.css';
 
 export const ThemeContext = React.createContext();
@@ -9,12 +9,19 @@ export default function App() {
   const [darkTheme, setDarkTheme] = useState(true);
   function toggleTheme() {
     setDarkTheme(prevDarkTheme => !prevDarkTheme);
+    if (darkTheme) {
+      document.body.style.backgroundColor = 'lightseagreen';
+      document.getElementById('butty').style.backgroundColor = 'navy';
+    } else {
+      document.body.style.backgroundColor = 'steelblue';
+      document.getElementById('butty').style.backgroundColor = 'skyblue';
+    }
   }
   return (
     <div className="App">
       <ThemeContext.Provider value={darkTheme} >
       <div className="wrapper">
-        <button onClick={toggleTheme}>Toggle Theme</button>
+        <button id='butty' onClick={toggleTheme}>Toggle Theme</button>
         <FunctionComponentWithContext />
         <ClassComponentWithContext />
       </div>
