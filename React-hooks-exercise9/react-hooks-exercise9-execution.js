90dc51aa60e8899f0dd1b9371277b575daaef27c
Johan Swan <53649803+JohanSwannie@users.noreply.github.com>
2022-11-17 08:55:35 +1100

    Update react-hooks-exercise9-execution.js

--- React-hooks-exercise9/react-hooks-exercise9-execution.js
+++ React-hooks-exercise9/react-hooks-exercise9-execution.js
@@ -5,7 +5,7 @@ export default function Execution({ execution, dispatch }) {
 
   return (
     <div>
-      <span style={{fontSize: '21px', color: execution.done ? 'navy' : 'crimson'}}>{execution.name}</span>
+      <span style={{fontSize: '28px', fontWeight: 'bold', color: execution.done ? 'navy' : 'crimson', fontFamily: execution.done ? 'Sofia' : 'Tangerine'}}>{execution.name}</span>
       <div style={{display: 'grid', gridTemplateColumns: 'repeat(2, 1fr)', gridColumnGap: '4%', marginLeft: '28%'}}>
         <button onClick={() => dispatch({ type: ACTIONS.TOGGLE_EXECUTION, loading: { id: execution.id }})}>Toggle</button>
         <button onClick={() => dispatch({ type: ACTIONS.DELETE_EXECUTION, loading: { id: execution.id }})}>Delete</button>
