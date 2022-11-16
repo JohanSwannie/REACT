2344de885b94c6120a3930e4e6d700d08061716a
Johan Swan <53649803+JohanSwannie@users.noreply.github.com>
2022-11-16 14:18:50 +1100

    Create react-hooks-exercise9-app.js

---
+++ React-hooks-exercise9/react-hooks-exercise9-app.js
@@ -0,0 +1,53 @@
+import React, { useState, useReducer } from 'react';
+import Todo from './Todo';
+import './App.css';
+
+export const ACTIONS = {
+  ADD_EXECUTION: 'add_execution',
+  TOGGLE_EXECUTION: 'toggle_execution',
+  DELETE_EXECUTION: 'delete_execution'
+};
+
+function reducer(executions, action) {
+  switch (action.type) {
+    case ACTIONS.ADD_EXECUTION:
+      return [...executions, newTodo(action.loading.name)];
+    case ACTIONS.TOGGLE_EXECUTION:
+      return executions.map(execution => {
+        if (execution.id === action.loading.id) {
+          return {...execution, done: !execution.done};
+        }
+        return execution;
+      })
+      case ACTIONS.DELETE_EXECUTION:
+        return executions.filter(execution => execution.id !== action.loading.id);
+    default:
+      return executions;
+  }
+}
+
+function newTodo(name) {
+  return { id: Date.now(), name: name, done: false };
+}
+
+export default function App() {
+  const [executions, dispatch] = useReducer(reducer, []);
+  const [name, setName] = useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    dispatch({ type: ACTIONS.ADD_EXECUTION, loading: {name: name} });
+    setName('');
+  }
+
+  return (
+    <div className="App">
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={name} placeholder="Enter Name" onChange={event => setName(event.target.value)} />
+      </form>
+        {executions.map(execution => {
+          return <Todo key={execution.id} execution={execution} dispatch={dispatch} />
+      })}
+    </div>
+  );
+}
