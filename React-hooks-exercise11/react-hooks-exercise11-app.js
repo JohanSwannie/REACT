0f896d65b0ec20678d65b3cc3fea595744d82140
Johan Swan <53649803+JohanSwannie@users.noreply.github.com>
2022-11-18 14:32:14 +1100

    Create react-hooks-exercise11-app.js

---
+++ React-hooks-exercise11/react-hooks-exercise11-app.js
@@ -0,0 +1,31 @@
+import React, { useState, useTransition } from 'react';
+import './App.css';
+
+export default function App() {
+  const [isPending, startTransition] = useTransition();
+  const [myInput, setMyInput] = useState('');
+  const [array, setArray] = useState([]);
+
+  const ARRAY_LENGTH = 10000;
+
+  function handleChange(event) {
+    event.preventDefault();
+    setMyInput(event.target.value);
+    startTransition(() => {
+      const list = [];
+      for (var i = 0; i < ARRAY_LENGTH; i++) {
+        list.push(event.target.value);
+      }
+      setArray(list);
+    });
+  }
+  return (
+    <div className="App">
+      <input type="text" value={myInput} onChange={handleChange} />
+      {isPending ? "Input Loading..."
+       : array.map((element, index) => {
+         return <div key={index} style={{fontSize: '40px', color: 'red', fontFamily: 'Tangerine'}}>{element}</div>
+       })}
+    </div>
+  );
+}
