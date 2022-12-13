b299f91aab8a6fa904feee36abee766c9078ec67
Johan Swan <53649803+JohanSwannie@users.noreply.github.com>
2022-12-13 13:27:12 +1100

    Create user-interface1-App.js

---
+++ React-User-Interface1/user-interface1-App.js
@@ -0,0 +1,74 @@
+import { useState, useEffect, useCallback } from 'react';
+import { FaBuilding } from 'react-icons/fa';
+import SearchCompanies from './components/SearchCompanies';
+import AddCompany from './components/AddCompany';
+import CompanyInfo from './components/CompanyInfo';
+
+function App() {
+  let [companyDetailList, setCompanyDetailList] = useState([]);
+  let [searchquery, setSearchQuery] = useState("");
+  let [sortBy, setSortBy] = useState("companyName");
+  let [orderBy, setOrderBy] = useState("asc");
+
+  const filteredCompanyDetails = companyDetailList.filter(
+    item => {
+      return (
+        item.companyName.toLowerCase().includes(searchquery.toLowerCase()) ||
+        item.CEO.toLowerCase().includes(searchquery.toLowerCase()) ||
+        item.companyLocation.toLowerCase().includes(searchquery.toLowerCase())
+      );
+    }
+  ).sort((a, b) => {
+    let order = (orderBy === "asc") ? 1: -1;
+    return (
+      a[sortBy].toLowerCase() < b[sortBy].toLowerCase() ? -1 * order : 1 * order
+    );
+  });
+
+  const fetchData = useCallback(() => {
+    fetch('./inputdata.json')
+    .then(response => response.json())
+    .then(data => {
+      setCompanyDetailList(data);
+    });
+  }, []);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
+  return (
+    <div className="App container mx-auto mt-3 font-thin">
+      <h1 className="text-5xl mb-3 main_heading">
+        <FaBuilding className=" inline-block align-top" />
+         Top Ranked IT Companies - Details
+        <FaBuilding className=" inline-block align-top" /></h1>
+      <AddCompany
+        onDispatchCompanyInfo={newCompany => setCompanyDetailList([...companyDetailList, newCompany])}
+        lastId={companyDetailList.reduce((max, nextId ) => Number(nextId.id) > max ? Number(nextId.id) : max, 0)}
+       />
+      <SearchCompanies searchquery={searchquery}
+      onSeacrhQueryChange={mySearchQuery => setSearchQuery(mySearchQuery)}
+      orderby={orderBy}
+      onOrderByChange={newSortOrder => setOrderBy(newSortOrder)}
+      sortBy={sortBy}
+      onSortByChange={newSort => setSortBy(newSort)}
+      />
+      <ul className="divide-y divide-gray-200">
+      {filteredCompanyDetails
+        .map(companyDetail => (
+          <CompanyInfo key={companyDetail.id}
+          companyDetail={companyDetail}
+          onDeleteCompanyDetail = {
+            companyDetailId => {
+              setCompanyDetailList(companyDetailList.filter(companyDetail => companyDetail.id !== companyDetailId))
+            }
+          }
+          />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
