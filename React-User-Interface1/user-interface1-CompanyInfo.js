42bdf3a64cb50126d9c114804f54842f45fd59e5
Johan Swan <53649803+JohanSwannie@users.noreply.github.com>
2022-12-14 09:13:43 +1100

    Update user-interface1-CompanyInfo.js

--- React-User-Interface1/user-interface1-CompanyInfo.js
+++ React-User-Interface1/user-interface1-CompanyInfo.js
@@ -7,9 +7,8 @@ const CompanyInfo = ( {companyDetail, onDeleteCompanyDetail} ) => {
         className="p-1.5 mr-1.5 mt-1 rounded text-white bg-gradient-to-r from-violet-500 to-fuchsia-500 hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
         <BiTrash /></button>
       <div className="flex-grow">
-        <div className="flex items-center">
-          <span className="flex-none font-medium text-2xl text-blue-500">Company Name: {companyDetail.companyName}</span>
-          <span className="flex-grow text-right">{companyDetail.companyStartDate}</span>
+        <div className="flex items-center">          <span className="flex-none font-medium text-2xl text-blue-500">Company Name: {companyDetail.companyName}</span>
+          <span className="flex-grow text-right"><span className="font-medium text-1xl text-blue-500">Company Start Date & Time : </span>{companyDetail.companyStartDate}</span>
         </div>
         <div><b className="font-bold text-blue-500">CEO: </b>{companyDetail.CEO}</div>
         <div><b className="leading-tight font-bold text-blue-500">Company Location: </b>{companyDetail.companyLocation}</div>
