d0f3e0731b01216e90a80d7ce15e7bc5bccc8da9
Johan Swan <53649803+JohanSwannie@users.noreply.github.com>
2022-12-21 14:39:36 +1100

    Create App.js

---
+++ React-Beginners-Content/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import HooksDisplay1 from './components/HooksDisplay1';
+import HooksDisplay2 from './components/HooksDisplay2';
+import HooksDisplay3 from './components/HooksDisplay3';
+import HooksDisplay4 from './components/HooksDisplay4';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        REACT HOOKS EXERCISE
+        <HooksDisplay1 />
+        <HooksDisplay2 />
+        <HooksDisplay3 />
+        <HooksDisplay4 />
+      </header>
+    </div>
+  );
+}
+
+export default App;
