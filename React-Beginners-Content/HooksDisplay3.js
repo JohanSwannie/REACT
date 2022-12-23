488993787fd8eda6088eea5aa2609154725bda9f
Johan Swan <53649803+JohanSwannie@users.noreply.github.com>
2022-12-23 13:54:49 +1100

    Update HooksDisplay3.js

--- React-Beginners-Content/HooksDisplay3.js
+++ React-Beginners-Content/HooksDisplay3.js
@@ -5,12 +5,26 @@ const reducerFunc = (state, action) => {
     case "INCREMENT":
       return {
         counter: state.counter + 1,
-        showText: state.showText
+        showText1: state.showText1,
+        showText2: state.showText2
       };
-    case "TOGGLESHOWTEXT":
+    case "DECREMENT":
+      return {
+        counter: state.counter - 1,
+        showText2: state.showText2,
+        showText1: state.showText1
+      };
+    case "TOGGLESHOWTEXT1":
       return {
         counter: state.counter,
-        showText: !state.showText
+        showText1: !state.showText1,
+        showText2: state.showText2
+      };
+    case "TOGGLESHOWTEXT2":
+      return {
+        counter: state.counter,
+        showText2: !state.showText2,
+        showText1: state.showText1
       };
     default:
       return state;
@@ -19,17 +33,27 @@ const reducerFunc = (state, action) => {
 
 const HooksDisplay3 = () => {
 
-  const [state, dispatch] = useReducer(reducerFunc, {counter: 0, showText: true});
+  const [state, dispatch] = useReducer(reducerFunc, {counter: 0, showText1: true, showText2: true});
 
   return (
     <div className="hooks3">
-    <h2>Hooks Display 3</h2>
-    <h3>{state.counter}</h3>
-      <button onClick={() => {
-       dispatch({ type: "INCREMENT" });
-       dispatch({ type: "TOGGLESHOWTEXT"});
-     }}>CLICK ME</button>
-     {state.showText && <p>Text is now Showing</p>}
+      <h2>Hooks 3 Display</h2>
+      <h3>{state.counter}</h3>
+        <button onClick={() => {
+         dispatch({ type: "INCREMENT" });
+         dispatch({ type: "TOGGLESHOWTEXT1"});
+       }}>Increment Counter</button>
+       {state.showText1 && <p>Clicked 1</p>}
+      <h3>{state.counter}</h3>
+        <button onClick={() => {
+         dispatch({ type: "DECREMENT" });
+         dispatch({ type: "TOGGLESHOWTEXT2"});
+       }}>Decrement Counter</button>
+       {state.showText2 && <p>Clicked 2</p>}
+      <h1>Toggle 1 Show Text Message</h1>
+      {state.showText1 && <p>Incrementation Clicked 1</p>}
+      <h1>Toggle 2 Show Text Message</h1>
+      {state.showText2 && <p>Decrementation Clicked 2</p>}
     </div>
   );
 }
