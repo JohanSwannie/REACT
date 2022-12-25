eb7bd81108834189a4d46a6784273b4ad376c7a8
Johan Swan <53649803+JohanSwannie@users.noreply.github.com>
2022-12-26 08:38:50 +1100

    Create Card-Searchbox-SearchBox.js

---
+++ React-Small-Projects/Card-Searchbox/Card-Searchbox-SearchBox.js
@@ -0,0 +1,10 @@
+const Card-Searchbox-SearchBox = ({ className, placeholder, onChangeHandler }) => (
+  <input
+    className={`search-box ${className}`}
+    type='search'
+    placeholder={placeholder}
+    onChange={onChangeHandler}
+  />
+);
+
+export default Card-Searchbox-SearchBox;
