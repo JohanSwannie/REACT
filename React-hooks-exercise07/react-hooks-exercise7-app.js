17321613f8acfdf9efd710d389ebb7935bc6aca7
Johan Swan <53649803+JohanSwannie@users.noreply.github.com>
2022-12-28 12:17:48 +1100

    Update react-hooks-exercise7-app.js

--- React-hooks-exercise07/react-hooks-exercise7-app.js
+++ React-hooks-exercise07/react-hooks-exercise7-app.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import FunctionContext from './FunctionContext';
 import { ConceptProvider } from './ConceptContext';
-import './App.css';
 
 export default function App() {
   return (
