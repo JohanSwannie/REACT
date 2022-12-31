5e6f38ff82a06f7b8c2e66f7a4d02b005fa03f00
Johan Swan <53649803+JohanSwannie@users.noreply.github.com>
2023-01-01 09:27:50 +1100

    Update react-hooks-exercise9-execution.js

--- React-hooks-exercise09/react-hooks-exercise9-execution.js
+++ React-hooks-exercise09/react-hooks-exercise9-execution.js
@@ -4,7 +4,7 @@ export default function Execution({ execution, dispatch }) {
 
   return (
     <div>
-      <div style={{display: 'grid', gridTemplateColumns: 'repeat(5, 12fr)', margin: 'auto'}}>
+      <div style={{display: 'grid', gridTemplateColumns: 'repeat(5, 1fr)', margin: 'auto'}}>
           <button className='hide'></button>
           <button className='show' onClick={() => dispatch({ type: ACTIONS.TOGGLE_EXECUTION, stack: { id: execution.id }})}>Toggle</button>
           <span style={{width: '70vw', fontSize: '37px', fontWeight: 'bold', textAlign: 'center', color: execution.done ? 'navy' : 'crimson', fontFamily: execution.done ? 'Sofia' : 'Tangerine'}}>{execution.enteredText}</span>
