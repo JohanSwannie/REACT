5df7cf0a2cfef7602fc6f755bc191040f0fed1ca
Johan Swan <53649803+JohanSwannie@users.noreply.github.com>
2023-01-01 21:58:26 +1100

    Update react-hooks-exercise12-DataArray.js

--- React-hooks-exercise12/react-hooks-exercise12-DataArray.js
+++ React-hooks-exercise12/react-hooks-exercise12-DataArray.js
@@ -1,3 +1,13 @@
+import eknmPiece  from './Eine-Kleine-Nachtmusik.mp3';
+import fuPiece from './Fur-Elise.mp3';
+import ombcPiece from './O-Mio-Babbino-Caro.mp3';
+import tfPiece from './Toccata-Fugue-D-minor.mp3';
+import fsPiece from './Vivaldi–four-Seasons.mp3';
+import cPiece from './Carmen-Bizet.mp3';
+import bdPiece from './Blue-Danube.mp3';
+import boPiece from './Bolero.mp3';
+
+
 export default function DataArray({dataArray}) {
 
   const dataArrayStyle = {
@@ -25,6 +35,14 @@ export default function DataArray({dataArray}) {
             <li key={index}><em>Music Piece  </em><b>{JSON.stringify(item.name)}</b>
                 <em>Composer  </em><b>{JSON.stringify(item.composer)}</b>
                 <em>Composed  </em><b>{JSON.stringify(item.year)}</b>
+                {index === 0 && <audio src={eknmPiece} controls />}
+                {index === 1 && <audio src={fuPiece} controls />}
+                {index === 2 && <audio src={ombcPiece} controls />}
+                {index === 3 && <audio src={tfPiece} controls />}
+                {index === 4 && <audio src={fsPiece} controls />}
+                {index === 5 && <audio src={cPiece} controls />}
+                {index === 6 && <audio src={bdPiece} controls />}
+                {index === 7 && <audio src={boPiece} controls />}
             </li>
           <br></br>
         </ul>
