49f304053ba311784b7c7b755e840edb809b5a70
Johan Swan <53649803+JohanSwannie@users.noreply.github.com>
2023-01-01 17:56:04 +1100

    Update react-hooks-exercise12-RestExecutions.js

--- React-hooks-exercise12/react-hooks-exercise12-RestExecutions.js
+++ React-hooks-exercise12/react-hooks-exercise12-RestExecutions.js
@@ -10,12 +10,23 @@ export default function Executions({ execution, dispatch }) {
     dispatch({ type: ACTIONS.DELETE_EXECUTION, stack: { id: execution.id }});
   }
 
+  const textInputStyle = {
+    color: execution.done ? '#FFF' : 'crimson',
+    background: '#000',
+    fontSize: execution.done ? '25px' : '37px',
+    fontFamily: execution.done ? 'Sofia' : 'Tangerine',
+    fontWeight: 'bold',
+    width: '70vw',
+    textAlign: 'center',
+    border: '2px solid #FFF'
+  };
+
   return (
     <div>
       <div style={{display: 'grid', gridTemplateColumns: 'repeat(5, 1fr)', margin: 'auto'}}>
           <button className='hide'></button>
           <button className='show' onClick={toggleHim}>Toggle</button>
-          <p style={{width: '70vw', fontSize: '37px', fontWeight: 'bold', textAlign: 'center', color: execution.done ? 'navy' : 'crimson', fontFamily: execution.done ? 'Sofia' : 'Tangerine'}}>{execution.enteredText}</p>
+          <p style={textInputStyle}>{execution.enteredText}</p>
           <button className='show' onClick={deleteHim}>Delete</button>
           <button className='hide'></button>
       </div>
